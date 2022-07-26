fc6386d1c8e4def4ff0870622b129d10225cd1fc
zerotop <qk122.5.25@icloud.com>
2022-07-26 17:29:27 +0800

    rename module & update pom

--- scallion-gateway/src/main/java/top/zerotop/gateway/scalliongateway/ScallionGatewayApplication.java
+++ scallion-gateway/src/main/java/top/zerotop/gateway/scalliongateway/ScallionGatewayApplication.java
@@ -1,13 +1,13 @@
-package top.zerotop.auto;
+package top.zerotop.gateway.scalliongateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AutoApplication {
+public class ScapplionGatewayApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AutoApplication.class, args);
+        SpringApplication.run(ScapplionGatewayApplication.class, args);
     }
 
 }
--- scallion-gateway/src/test/java/top/zerotop/gateway/scappliongateway/ScallionGatewayApplicationTests.java
+++ scallion-gateway/src/test/java/top/zerotop/gateway/scappliongateway/ScallionGatewayApplicationTests.java
@@ -1,10 +1,10 @@
-package top.zerotop.auto;
+package top.zerotop.gateway.scappliongateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class AutoApplicationTests {
+class ScallionGatewayApplicationTests {
 
     @Test
     void contextLoads() {
