7eb0d9d8718fc62ade6d874070db0b2883316560
zerotop <qk122.5.25@icloud.com>
2022-07-27 20:55:47 +0800

    update pom dependency

--- scallion-gateway/src/main/java/top/zerotop/gateway/scalliongateway/ScallionGatewayApplication.java
+++ scallion-gateway/src/main/java/top/zerotop/gateway/scalliongateway/ScallionGatewayApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ScapplionGatewayApplication {
+public class ScallionGatewayApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ScapplionGatewayApplication.class, args);
+        SpringApplication.run(ScallionGatewayApplication.class, args);
     }
 
 }
