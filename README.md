2aa5530e35ee2f828d094d4eb164197a6c53949d
zerotop <qk122.5.25@icloud.com>
2022-08-03 14:36:18 +0800

    update pom version and add nacos client and connect to nacos server

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# springcloud-auto
-a spring cloud project
+# springcloud-scallion
+
+使用nacos做服务发现,gateway的springcloud项目
+
+# 技术栈
+
+java11
+
+## spring-cloud-alibaba
+
+nacos-server: 1.4.2
+    需要使用spring-cloud.alibaba.version=2.2.6.RELEASE
+    2.2.7.RELEASE 则对应nacos客户端2.x版本,主要是gRPC协议更新影响
--- scallion-web/web-example/src/main/java/top/zerotop/scallion/web/example/WebExampleApplication.java
+++ scallion-web/web-example/src/main/java/top/zerotop/scallion/web/example/WebExampleApplication.java
@@ -2,7 +2,9 @@ package top.zerotop.scallion.web.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class WebExampleApplication {
 
