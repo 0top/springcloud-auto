86c20289ee2ef5c6f3a8762f2f86dc0d4edb4643
zerotop <qk122.5.25@icloud.com>
2023-03-09 23:31:40 +0800

    add datasource config

--- scallion-task/scallion-task-auto/src/main/java/top/zerotop/scallion/task/auto/config/DataSourceConfig.java
+++ scallion-task/scallion-task-auto/src/main/java/top/zerotop/scallion/task/auto/config/DataSourceConfig.java
@@ -10,7 +10,7 @@ import javax.sql.DataSource;
 
 @Configuration
 public class DataSourceConfig {
-    @Bean
+    @Bean(name = "scallionTaskDataSource")
     @ConfigurationProperties("spring.datasource.scallion-task")
     public DataSource taskDataSource() {
         return DataSourceBuilder.create().build();
