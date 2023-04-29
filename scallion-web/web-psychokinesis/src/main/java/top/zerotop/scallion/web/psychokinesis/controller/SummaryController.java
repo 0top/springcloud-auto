2b22b45ec204a442cde7531a17f6ac41d7010435
zerotop <qk122.5.25@icloud.com>
2023-04-29 20:10:59 +0800

    update web psychokinesis

--- scallion-web/web-psychokinesis/src/main/java/top/zerotop/scallion/web/psychokinesis/controller/SummaryController.java
+++ scallion-web/web-psychokinesis/src/main/java/top/zerotop/scallion/web/psychokinesis/controller/SummaryController.java
@@ -1,6 +1,7 @@
 package top.zerotop.scallion.web.psychokinesis.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.*;
 import top.zerotop.common.rest.Response;
 import top.zerotop.common.rest.ResponseUtil;
@@ -14,14 +15,23 @@ import top.zerotop.scallion.web.psychokinesis.service.SummaryService;
 import java.util.List;
 
 @RestController
+//@RefreshScope
 @RequestMapping(value = "/task")
 public class SummaryController {
     @Autowired
     private SummaryService summaryService;
 
+//    @GetMapping(value = "/summary")
+//    private Response<Void> summaryData() {
+//        System.out.println("接到统计请求");
+//        summaryService.summarySentence();
+//        return ResponseUtil.ok();
+//    }
+
     @GetMapping(value = "/summary")
-    private Response<Void> summaryData() {
+    private String summaryData() {
+        System.out.println("接到统计请求");
         summaryService.summarySentence();
-        return ResponseUtil.ok();
+        return "ok";
     }
 }
---
+++ scallion-web/web-psychokinesis/src/main/java/top/zerotop/scallion/web/psychokinesis/dto/CommentDto.java
@@ -0,0 +1,101 @@
+package top.zerotop.scallion.web.psychokinesis.dto;
+
+import top.zerotop.scallion.web.psychokinesis.entity.Sentence;
+
+import java.io.Serializable;
+
+public class CommentDto implements Serializable {
+    private long id;
+
+    private String userId;
+
+    private String sentence;
+
+    private String createTime;
+
+    private String updateTime;
+
+    // 转发
+    private long repostsCount;
+
+    // 评论
+    public long commentsCount;
+
+    // 点赞
+    private long attitudesCount;
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getSentence() {
+        return sentence;
+    }
+
+    public void setSentence(String sentence) {
+        this.sentence = sentence;
+    }
+
+    public String getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(String createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(String updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public long getRepostsCount() {
+        return repostsCount;
+    }
+
+    public void setRepostsCount(long repostsCount) {
+        this.repostsCount = repostsCount;
+    }
+
+    public long getCommentsCount() {
+        return commentsCount;
+    }
+
+    public void setCommentsCount(long commentsCount) {
+        this.commentsCount = commentsCount;
+    }
+
+    public long getAttitudesCount() {
+        return attitudesCount;
+    }
+
+    public void setAttitudesCount(long attitudesCount) {
+        this.attitudesCount = attitudesCount;
+    }
+
+    public static CommentDto from(Sentence s) {
+        CommentDto dto = new CommentDto();
+        dto.setId(s.getId());
+        dto.setUserId(s.getUserId());
+        dto.setSentence(s.getSentence());
+        dto.setCreateTime(s.getCreateTime());
+        dto.setUpdateTime(s.getUpdateTime());
+        return dto;
+    }
+}
